e9f21c8cccd65a60c545a7132c673f351ed134a0
Natalya Potapova <natalya.potapova@elostech.cz>
2023-04-28 16:45:54 +0200

    adding comments, cycles to check the variable clean.sh; ssh-key and saving logs for user.script.sh

--- clean.sh
+++ clean.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
+USR=$1
 
-for (( i = 1; i <= $1; i++ ))
+if [ "$USR" = "0" ]; then 
+	#checks if the number of users is entered
+	echo "The operand must not be 0!"
+	exit 1;
+	#checks if the number of users is not 0
+	elif [ -z "$1" ]; then
+		echo "Operands errors"
+		exit 1;
+	else
+		#cycle for deleting a given number of users
+		for (( i = 1; i <= $USR; i++ ))
                 do
-                  userdel -r user$i
-                  echo "user$i smazan"
+                  userdel -r user$USR
+                  echo "user$USR deleted"
                 done
          exit 0;
+fi
 
 
--- user.script.sh
+++ user.script.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-usr=$1
-for ($i=1; $i<=$usr; $i++)
-do 
-	useradd name=user$usr password=student; 
-	su - user$usr;
-	ssh-keygen -y password " " user$usr
-	exit
-done
+USR=$1
+
+#checks if the number of users is entered
+if [ "$USR" = "0" ]; then
+	echo "Operand nesmi byt 0!"
+	exit 1;
+	#checks if the number of users is not 0
+	elif [ -z "$1" ]; then
+		echo "Chyby operand"
+		exit 1;
+	else
+
+		#the cycle of creating a given number of users
+		for (( i = 1; i <= $USR; i++ ))
+		do
+	         #create a user "user№" with the password "student" and a home directory
+		 useradd user$i --password "student" -m
+		 #generating ssh-keys for a user, without a password
+		 ssh-keygen -y password "/n" user$i
+		 #the function writes the number of the created user on stdin, sending the same message to the .log file and recording the date the file was created
+		 echo "user$i created" | tee -a test.$(date +%Y.%m.%d.%H:%M:%S).log
+	        done
+	 exit 0;	
+fi
+
+
 
 
 
